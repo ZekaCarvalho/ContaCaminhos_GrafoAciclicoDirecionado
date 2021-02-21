ee327211a9c3543b0f3dbb5146171311b825b116
ZekaCarvalho <smart.artdigital@gmail.com>
2021-02-21 19:38:36 -0300

    Ajustes

--- src/Controller/ContadorController.php
+++ src/Controller/ContadorController.php
@@ -10,16 +10,16 @@ use Symfony\Component\HttpFoundation\Request;
 class ContadorController extends AbstractController
 {
     /**
-     * @Route("/twig2", name="twig2")
+     * @Route("/contar-caminhos", name="contador")
      */
-    public function hello_twig2(Request $request): Response
+    public function main_contador(Request $request): Response
     {
         $grafo = $request->query->get('g');
-        $qtdDeCaminhos = $this->contaCaminhos($grafo, 0, count($grafo) - 1);
+        $qtdDeCaminhos = $this->conta_caminhos($grafo, 0, count($grafo) - 1);
         return $this->render('contador/index.html.twig', [ "qtdCaminhos" => $qtdDeCaminhos ]);
     } 
 
-    private function contaCaminhos($grafo, $noInicial, $noFinal){
+    private function conta_caminhos($grafo, $noInicial, $noFinal){
         $qtd_caminhos = 0;
         
         if($noInicial == $noFinal) return 1;
@@ -28,8 +28,11 @@ class ContadorController extends AbstractController
             // Verifica se existe caminho para o próximo vértice,
             // do contrário faz o backtracking.
             if ($grafo[$noInicial][$vertice] == 1)
-                $qtd_caminhos += $this->contaCaminhos($grafo, $vertice, $noFinal);
+                $qtd_caminhos += $this->conta_caminhos($grafo, $vertice, $noFinal);
         
         return $qtd_caminhos;
     }
 }
+
+//g[]=01101&g[]=00011&g[]=00001&g[]=00100&g[]=01101
+
